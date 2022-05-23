d33634d828eeb33fcc13b17d430f03c6b3aa67b0
Kleeritter <93885297+Kleeritter@users.noreply.github.com>
2022-05-23 18:28:49 +0200

    Update bessererdp.py

--- Übung_2/bessererdp.py
+++ Übung_2/bessererdp.py
@@ -4,7 +4,6 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 import numpy as np
 import random
-print(random.gauss(0,1))
 
 
 n= 1000#!Anzahl Partikel
@@ -14,8 +13,7 @@ zq =np.float64( 45) #!m
 xq =np.float64( 50) #!m
 counter=np.float64(0)
 xgrenz= np.float64(2000)# !m
-#posi=np.array([])
-#ges=np.array([])
+
 
 ges=[]
 tl = np.float64(100)  #s Zeit
@@ -23,9 +21,7 @@ dt = np.float64(4) # Zeitschritt
 sigu= np.float64(0) #m/s
 sigw= np.float64(0.39) #m/s
 rl= math.exp(- dt/tl)
-print(rl)
-xliste=[]
-zliste =[]
+
 
 for i in tqdm(range(n)):
     xi=xq
@@ -36,38 +32,24 @@ for i in tqdm(range(n)):
 
     while (xi<= xgrenz):
         if (zi<0):
-            #print("alarm")
             zi=-zi
             wi= -wi
             rr=np.float64( random.gauss(0,1))
-            #ui= rl*ui + math.sqrt((1 - rl**2))*sigu* rr
             ui=5
             xi= xi + ui*dt
             wi= rl*wi + math.sqrt((1 - rl**2))*sigw* rr
             zi= zi + wi*dt
-            #transport=np.array([xi,zi])
-            #posi=np.append(posi, (xi,zi))
-            #ges=np.append(ges,posi)
             posi.append([xi,zi])
-            xliste.append(ui)
-            zliste.append(wi)
+
 
         else:
             rr= np.float64(random.gauss(0,1))
-            #ui= rl*ui + math.sqrt((1 - rl**2))*sigu* rr
             ui=5
             xi= xi + ui*dt
             wi= rl*wi + math.sqrt((1 - rl**2))*sigw* rr
             zi= zi + wi*dt
-            #transport=np.array([xi,zi])
-            #posi=np.append(posi, (xi,zi))
-            #ges=np.append(ges,posi)
             posi.append([xi,zi])
-            xliste.append(ui)
-            zliste.append(wi)
-    #counter=counter +1
     ges.append(posi)
-print(ges[0])
 for i in tqdm(range(len(ges))):
     x=[]
     z=[]
@@ -75,8 +57,9 @@ for i in tqdm(range(len(ges))):
         x.append(ges[i][j][0])
         z.append(ges[i][j][1])
     plt.plot(x,z)
-    #x=np.arange(0,len(ges[i]))
-    #plt.plot(x,ges[i])
 
+plt.title("Partikeltrajektorien")
+plt.xlabel("Distanz  X in m")
+plt.ylabel("Höhe Z in m")
 plt.show()
 
